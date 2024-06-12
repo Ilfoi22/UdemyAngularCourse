0a321d13a1049bd43ccedbc4a3620ffcb0dbb651
Ilfoi22 <andrii.tsunivskii@gmail.com>
2024-06-12 16:42:46 +0300

    2024-06-12 add tasks view

--- easytask/src/app/app.component.ts
+++ easytask/src/app/app.component.ts
@@ -18,7 +18,13 @@ export class AppComponent {
 
   users = DUMMY_USERS;
 
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
   onSelectUser(id: string) {
-    
+    this.selectedUserId = id;
   }
 }
